2cc1c486973b52476cf252f9489c36300a34791b
afeiship <1290657123@qq.com>
2017-08-10 22:04:41 +0800

    issue: component

--- RnFlatList/index.ios.js
+++ RnFlatList/index.ios.js
@@ -9,45 +9,48 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableOpacity,
+  Platform,
+  Dimensions,
+  RefreshControl,
+  FlatList,
+  ActivityIndicator,
+  DeviceEventEmitter,
+  ScrollView
 } from 'react-native';
+import LeftFlatList from './components/flat-list';
+import linkageData from './components/data.json';
+
 
 export default class RnFlatList extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{flexDirection:'row'}}>
+               <LeftFlatList data = {linkageData}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('RnFlatList', () => RnFlatList);
